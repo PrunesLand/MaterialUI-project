ca724e29562eba3aee642048cf93144171af5079
Pranaya Anargya <panargya@gmail.com>
2021-06-24 17:50:29 +1000

    theme tutorial

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,11 +2,21 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+
+})
 
 export default function Create() {
+
+  const classes = useStyles()
+
   return (
     <div>
       <Typography
+
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -16,12 +26,18 @@ export default function Create() {
       </Typography>
 
       <Button
+
         onClick={() => console.log('You clicked me')}
         type="submit"
         color="secondary"
         variant="outlined"
+        startIcon={<AcUnitOutlinedIcon />}
         disableElevation
-      >Button</Button>
+      >
+        Button
+      </Button>
+
+      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
 
       <ButtonGroup color="secondary" variant="contained">
         <Button>One</Button>
