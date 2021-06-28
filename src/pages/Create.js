7373ff515089dbec6d512bf972068e90d5c45b9d
Pranaya Anargya <panargya@gmail.com>
2021-06-28 14:50:15 +1000

    finished tutorial 7

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
 import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles({
-
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
 })
 
 export default function Create() {
 
   const classes = useStyles()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
 
+    if (title && details) {
+      console.log(title, details)
+    }
+
+    if (title === '') {
+      setTitleError(true)
+    }
+    if (details === '') {
+      setDetailsError(true)
+    }
+  }
   return (
     <div>
       <Typography
@@ -25,17 +49,47 @@ export default function Create() {
         Create a new note
       </Typography>
 
-      <Button
-
-        onClick={() => console.log('You clicked me')}
-        type="submit"
-        color="secondary"
-        variant="outlined"
-        startIcon={<AcUnitOutlinedIcon />}
-        disableElevation
+      <form
+        noValidate
+        autoComplete='off'
+        onSubmit={handleSubmit}
       >
-        Button
-      </Button>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="note field"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <Button
+
+
+          type="submit"
+          color="secondary"
+          variant="outlined"
+          startIcon={<AcUnitOutlinedIcon />}
+          disableElevation
+        >
+          Button
+        </Button>
+      </form>
+
 
       <AcUnitOutlinedIcon color="secondary" fontSize="small" />
 
